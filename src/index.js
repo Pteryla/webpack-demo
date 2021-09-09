ca0a8a0892ecfcb49300b0bdabff5114e3aa767e
WongYichen <1573578457@qq.com>
2021-09-09 16:56:18 +0800

    回退处理

--- src/index.js
+++ src/index.js
@@ -1,56 +1,11 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.jpg";
-import Data from "./data/data.xml";
-import Notes from "./data/data.csv";
 
-// 自定义 JSON 模块 parser
-// https://webpack.docschina.org/configuration/module/#ruleparserparse
-import toml from "./data/data.toml";
-import yaml from "./data/data.yaml";
-import json from "./data/data.json5";
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
-
-function componentHello() {
+function component() {
   const element = document.createElement("div");
-  const imageElementContainer = document.createElement("div");
 
-  // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-  // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  imageElementContainer.appendChild(myIcon);
 
   return element;
 }
 
-function componentImage() {
-  const imageElementContainer = document.createElement("div");
-
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image(200, 200);
-  myIcon.src = Icon;
-
-  imageElementContainer.appendChild(myIcon);
-
-  return imageElementContainer;
-}
-
-document.body.appendChild(componentHello());
-document.body.appendChild(componentImage());
-
-console.log(Data);
-console.log(Notes);
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
 
 module.exports = {
   mode: "production",
@@ -9,61 +6,6 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-        /*  模块 loader 可以链式调用。链中的每个 loader 都将对资源进行转换。
-            链会逆序执行。第一个 loader 将其结果（被转换后的资源）传递给下一个 loader，依此类推。
-            最后，webpack 期望链中的最后的 loader 返回 JavaScript。
-            应保证 loader 的先后顺序：'style-loader' 在前，而 'css-loader' 在后。如果不遵守此约定，webpack 可能会抛出错误。
-        */
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      /*
-            此外，可以加载的有用资源还有数据，如 JSON 文件，CSV、TSV 和 XML。
-            类似于 NodeJS，JSON 支持实际上是内置的，也就是说 import Data from './data.json' 默认将正常运行。
-            要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader。让我们处理加载这三类文件：
-    */
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
   },
 };
 
